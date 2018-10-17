b72f9bf6537d19a3eabd89b613873c3516505e5c
차 차 <aoikazto@naver.com>
2018-10-18 00:44:26 +0900

    last update

---
+++ DEU/DEUProject/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DEUProject
+{
+    public partial class MainPage : TabbedPage
+    {
+        MainViewModel viewModel;
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MainPage()
+        {
+            BindingContext = viewModel = new MainViewModel();
+
+            InitializeComponent();
+        }
+    }
+}
