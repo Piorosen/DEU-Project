3aaa5796ae8ec56168e8d962c4a349f2dcd26985
aoikazto@gmail.com <aoikazto@naver.com>
2018-11-01 22:05:04 +0900

    Icon Changed.

--- DEU/DEUProject.Android/Resources/Resource.designer.cs
+++ DEU/DEUProject.Android/Resources/Resource.designer.cs
@@ -3944,10 +3944,13 @@ namespace DEUProject.Droid
 			public const int icon = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int icon_round = 2130903041;
+			public const int Icon2 = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int launcher_foreground = 2130903042;
+			public const int icon_round = 2130903042;
+			
+			// aapt resource value: 0x7f030003
+			public const int launcher_foreground = 2130903043;
 			
 			static Mipmap()
 			{
